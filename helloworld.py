ae4d7a31f82c5d74b0fc860646f6b857ebdde23f
Andres Cea <makseaa@gmail.com>
2023-09-13 17:06:01 -0700

    updated the message sent to the function as Hi Mac

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+#print('Hello World') #This is the first script created by me: Mac Cea
+messages= ['HelloWorld','Hi Mac','This is a wonderful World']
+def ShowingMessage(message) :
+    for x in messages:
+        if message in x:
+            print(x)
+
+ShowingMessage('Hi Mac')
